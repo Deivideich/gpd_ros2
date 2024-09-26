9792604e4d50fe27fb70b29305688a74fdeec8fe
deivideich <david_vl2003@hotmail.com>
2024-09-26 07:28:49 +0000

    Grasp messages.cpp works as a shared library, still needs tests

--- gpd_ros2_executables/include/gpd_ros2_executables/grasp_messages.h
+++ gpd_ros2_executables/include/gpd_ros2_executables/grasp_messages.h
@@ -1,49 +1,28 @@
-/*
- * Software License Agreement (BSD License)
- *
- *  Copyright (c) 2018, Andreas ten Pas
- *  All rights reserved.
- *
- *  Redistribution and use in source and binary forms, with or without
- *  modification, are permitted provided that the following conditions
- *  are met:
- *
- *   * Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *   * Redistributions in binary form must reproduce the above
- *     copyright notice, this list of conditions and the following
- *     disclaimer in the documentation and/or other materials provided
- *     with the distribution.
- *
- *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
- *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- *  POSSIBILITY OF SUCH DAMAGE.
- */
-
 #ifndef GRASP_MESSAGES_H_
 #define GRASP_MESSAGES_H_
 
-#include <eigen_conversions/eigen_msg.h>
-
-#include <gpd/candidate/hand.h>
-
-#include <gpd_ros2/GraspConfig.h>
-#include <gpd_ros2/GraspConfigList.h>
+#include <Eigen/Dense>
+#include <geometry_msgs/msg/vector3.hpp>
+#include <gpd_ros2_msgs/msg/grasp_config.hpp>
+#include <gpd_ros2_msgs/msg/grasp_config_list.hpp>
+#include <gpd/candidate/hand.h>  // Include the correct header for Hand
+#include <std_msgs/msg/header.hpp>  // This line is crucial
 
 namespace GraspMessages
 {
-  gpd_ros2::GraspConfigList createGraspListMsg(const std::vector<std::unique_ptr<gpd::candidate::Hand>>& hands, const std_msgs::Header& header);
+    geometry_msgs::msg::Vector3 convertToVectorMsg(const Eigen::Vector3d& vec) {
+        geometry_msgs::msg::Vector3 msg;
+        msg.x = vec.x();
+        msg.y = vec.y();
+        msg.z = vec.z();
+        return msg;
+    }
 
-  gpd_ros2::GraspConfig convertToGraspMsg(const gpd::candidate::Hand& hand);
+    gpd_ros2_msgs::msg::GraspConfigList createGraspListMsg(
+        const std::vector<std::unique_ptr<gpd::candidate::Hand>>& hands,
+        const std_msgs::msg::Header& header);
+    
+    gpd_ros2_msgs::msg::GraspConfig convertToGraspMsg(const gpd::candidate::Hand& hand);
 };
 
 #endif /* GRASP_MESSAGES_H_ */
